27b1d2ee4f7dea9b57b1f10f29b56b791deaaa4a
ChanceNCounter <ChanceNCounter@icloud.com>
2022-05-15 11:30:18 -0700

    remove oopsies from goal script

--- Assets/Prefabs/Goal/hitGoal.cs
+++ Assets/Prefabs/Goal/hitGoal.cs
@@ -5,23 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class hitGoal : MonoBehaviour
 {
-    [SerializeField] public string nextSceneName;
-    // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.name == "Frog")
         {
             Debug.Log("You win!");
-            // Need to change to the "next" scene. Don't have that next part set up yet.
-            // int levelIndex = SceneManager.GetActiveScene().buildIndex;
             Scene currentLevel = SceneManager.GetActiveScene();
             other.enabled = false;
-            // Debug.Log(levelIndex);
-            // SceneManager.LoadScene(levelIndex + 1, LoadSceneMode.Additive);
-            // SceneManager.UnloadSceneAsync(levelIndex - 1, UnloadSceneOptions.None);
-
-            // SceneManager.LoadScene(nextSceneName, LoadSceneMode.Additive);
-            // SceneManager.UnloadSceneAsync(currentLevel.buildIndex - 1);
             FindObjectOfType<GameManager>().GoNextLevel();
         }
     }
