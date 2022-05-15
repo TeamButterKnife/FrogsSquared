177464ceb832a71baa6b331600ef6083177fccc3
ChanceNCounter <ChanceNCounter@icloud.com>
2022-05-14 20:52:20 -0700

    add global time limit, attach to sky script

--- Assets/Prefabs/LevelManager/LevelManager.cs
+++ Assets/Prefabs/LevelManager/LevelManager.cs
@@ -4,10 +4,8 @@ using UnityEngine;
 public class LevelManager : MonoBehaviour
 {
     [SerializeField] GlobalTimekeeper gameClock;
-    public DateTime GetStartTime()
-    {
-        return gameClock.GameStartTime;
-    }
+    public DateTime GetStartTime() => gameClock.GameStartTime;
+    public float GetTimeLimit() => gameClock.GameTimeLimit;
     private void Awake()
     {
         Debug.Log(gameClock.GameStartTime);
